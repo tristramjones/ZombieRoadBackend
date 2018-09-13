7dafdce01b87bcc9ce416cb505b4f6f6b3295b70
trisjones <trisjones04@gmail.com>
2018-09-13 11:45:47 -0400

    Added functions to both controllers to permit user/game creation and persistence.

--- app/controllers/api/v1/games_controller.rb
+++ app/controllers/api/v1/games_controller.rb
@@ -4,14 +4,23 @@ module Api
     class GamesController < ApplicationController
 
       def index
-        # all hobby objs loaded with their cats vs. 1 query per hobby
-        # do both and take pic of terminal
-        # render json: Hobby.all, include: ['cats']
         render json: Game.includes(:user), include: ['users']
+      end
+
+      def create
+        game = Game.create(game_params)
+        render json: game, status: 201
       end
 
       def show
       end
+
+      private
+
+      def game_params
+        params.permit(:score, :user_id)
+      end
+      
     end
   end
 end
--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -7,14 +7,19 @@ module Api
         render json: User.includes(:games), include: ['games']
       end
 
+      def create
+        user = User.create(user_params)
+        render json: user, status: 201
+      end
+
       def show
       end
 
-      # private
-      #
-      # def user_params
-      #   params.permit(:name)
-      # end
+      private
+
+      def user_params
+        params.permit(:name)
+      end
 
     end
   end
